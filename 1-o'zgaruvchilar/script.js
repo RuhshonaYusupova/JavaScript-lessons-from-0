17181db1178a4afde88b72d9d03fd2b2b7c22098
RuhshonaYusupova <yuspovar@gmail.com>
2022-05-04 19:31:55 -0700

    1-lesson

--- 1-o'zgaruvchilar/script.js
+++ 1-o'zgaruvchilar/script.js
@@ -1,15 +1,15 @@
-// console.log(d)
-// var d = 5
+console.log(d)
+var d = 5
 
-// console.log(c)
-// let c = 5
+console.log(c)
+let c = 5
 
-// let b = 5
-// b = 10
-// console.log(b)
+let b = 5
+b = 10
+console.log(b)
 
-// const a = 5
-// a = 10
-// console.log(a)
+const a = 5
+a = 10
+console.log(a)
 
 
