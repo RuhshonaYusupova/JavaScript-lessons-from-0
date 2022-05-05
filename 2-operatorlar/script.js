a0d6acd3daeb8c01d7693671ec17484c229bab58
RuhshonaYusupova <yuspovar@gmail.com>
2022-05-04 19:36:16 -0700

    2-lesson operatorlar

---
+++ 2-operatorlar/script.js
@@ -0,0 +1,118 @@
+
+
+
+let a = 5
+let b = 2
+// Arifmetik operatorlar
+// qo'shish
+console.log(a + b)
+// ayirish
+console.log(a - b)
+// ko'paytirish
+console.log(a * b)
+// darajaga ko'tarish
+console.log(a ** b)
+// bo;lish
+console.log(a / b)
+// qoldiqli bo'lish qoldiqni oladi
+console.log(a % b)
+// increment 
+console.log(a++)
+console.log(a)
+// decrement
+console.log(b--)
+console.log(b) 
+
+
+
+
+// belgilash operatorlari
+// 1.( = )	  a = b    a = b
+// 2.( += )	  a += b	 a = a + b
+// 3.( -= )	  a -= b	 a = a - b
+// 4.( *= )	  a *= b	 a = a * b
+// 5.( /= )	  a /= b	 a = a / b
+// 6.( %= )	  a %= b	 a = a % b
+// 7.( **= )	a **=b	 a =ax *b 
+let s = 5
+    b = 2
+    // 2
+    c = 10
+    d = 20
+    // 3
+    e = 100
+    f = 90
+    // 4
+    g = 5
+    h = 2
+    // 5
+    i = 40
+    j = 10
+    // 6
+    k = 10
+    l = 9
+    // 7
+    m =10
+    n = 2
+// 1    
+console.log(s = b )
+// 2
+console.log(c += d)
+// 3
+console.log(e -= f)
+// 4
+console.log(g *= h)
+// 5
+console.log(i /= j)
+// 6
+console.log(k %= l)
+// 7
+console.log(m **= n)
+
+
+
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+console.log(x)
+console.log(y)
+console.log(z)
+
+// 10
+// 55
+// Hello5
+
+
+
+// Taqqoslash operatorlari
+// ( == )	tenglik
+// ( === )	tenglik (typei bilan)
+// ( != )	teng emas
+// ( !== )	teng emas (typi bilan)
+// ( > )	katta
+// ( < )	kichik
+// ( >=	) katta yoki teng
+// ( <=)	kichik yoki teng
+// ( ?)	ternary operator
+
+
+let r = 5
+    b = '5'
+    c = 10
+    d = 20
+console.log(r == d)
+console.log(r === d)
+console.log(c != d)
+console.log(c !== d)
+console.log(c > d)
+console.log(c < d)
+console.log(c >= d)
+console.log(c <= d)
+console.log(a ? b: console.log(a))
+
+
+
+// Mantiqiy operatorlar
+// &&	logical and
+// ||	logical or
+// !	logical not
